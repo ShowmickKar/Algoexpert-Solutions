0a78a46a48d07c664ceb7df2cc9d457e4bddcb41
Showmick Kar <showmick12@gmail.com>
2022-01-03 15:39:58 +0600

    Update branch_sums.py

--- branch_sums.py
+++ branch_sums.py
@@ -1,4 +1,4 @@
-	# Problem Link: https://www.algoexpert.io/questions/Branch%20Sums
+# Problem Link: https://www.algoexpert.io/questions/Branch%20Sums
 
 class BinaryTree:
     def __init__(self, value):
@@ -8,15 +8,15 @@ class BinaryTree:
 
 
 def branchSumsHelper(root, cur_sum, ans):
-	if root == None:
-		return
-	if root.left == None and root.right == None:
-		ans.append(cur_sum + root.value)
-		return		
-	branchSumsHelper(root.left, cur_sum + root.value, ans)
-	branchSumsHelper(root.right, cur_sum + root.value, ans)		
+    if root == None:
+	return
+    if root.left == None and root.right == None:
+	ans.append(cur_sum + root.value)
+	return		
+    branchSumsHelper(root.left, cur_sum + root.value, ans)
+    branchSumsHelper(root.right, cur_sum + root.value, ans)		
 		
 def branchSums(root):
-    	ans = []
-    	branchSumsHelper(root, 0, ans)
-	return ans
+    ans = []
+    branchSumsHelper(root, 0, ans)
+    return ans
