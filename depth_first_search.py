6ef401c9d6c91a4c0648d51de7cb2380752a1d6e
Showmick Kar <showmick12@gmail.com>
2022-01-06 20:53:04 +0600

    Create depth_first_search.py

---
+++ depth_first_search.py
@@ -0,0 +1,21 @@
+# Problem Link: https://www.algoexpert.io/questions/Depth-first%20Search
+
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+		for child in self.children:
+			self = child
+			self.depthFirstSearch(array)
+		return array
